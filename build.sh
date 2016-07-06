798bb812c1e0acf3ee9d20335cffba74330b3354
Νικόλαος Κυριάκος Φυτίλης <n-fit@live.com>
2016-07-06 18:20:55 +0300

    oups

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 mkdir ../built
+for i in `cat ./order`
+  do a=$(($a+1))
+done
+
 for i in `cat ./order` 
-  do cd $i ; makepkg -si ; echo $i $? >> ../../built/built.log
+  do cd $i ; echo building $i , $a left to build
+  makepkg -si --noconfirm > /dev/null 2> /dev/null ; echo $i $? >> ../../built/built.log
   mv *pkg.tar* ../../built  
-  makepkg --clean --nobuild --nodeps  --noextract ; rm *.xz
+  makepkg --clean --nobuild --nodeps  --noextract > /dev/null 2> /dev/null ; rm *.xz
   cd ..
+  a=$(($a-1))
 done
