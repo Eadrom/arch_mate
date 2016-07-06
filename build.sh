053a6c78642419e9191e701e3abea70dfa3582c5
Nikos Fytilis <n-fit@live.com>
2016-07-06 12:46:31 +0000

    rm the xzs

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 mkdir ../built
 for i in `cat ./order` 
-  do cd $i ; makepkg -si 
-  makepkg --clean --nobuild --nodeps  --noextract
+  do cd $i ; makepkg -si ; echo $i $? >> ../../built/built.log
   mv *pkg.tar* ../../built  
+  makepkg --clean --nobuild --nodeps  --noextract ; rm *.xz
   cd ..
 done
