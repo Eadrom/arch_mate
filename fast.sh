02a0186e4b4098dbadac6ec16e996f65d194823f
Νικόλαος Κυριάκος Φυτίλης <n-fit@live.com>
2016-07-13 19:22:02 +0300

    probably fine

--- fast.sh
+++ fast.sh
@@ -1,4 +1,5 @@
-b='pkgname="${_pkgbase}-${_ver}-gtk3"' ; for i in `cat ../list` ; do a=$(cat $i/PKGBUILD | grep 'pkgname="${_pkgbase}"') ; if [ ! -z $a ] ; then sed -i -e "s/$a/$b/g" $i/PKGBUILD ; fi ; done
-b='"' ; c='-1.15-gtk3"' ; for i in caja-extensions-common caja-gksu caja-image-converter caja-open-terminal caja-sendto caja-share ; do sed -i -e "s/$i/$b$i$c/g" ./caja-extensions-1.15-gtk3/PKGBUILD ; done ; sed -i -e "s/package_\"/package_/g" ./caja-extensions-1.15-gtk3/PKGBUILD ; sed -i -e 's/"()/()/g' ./caja-extensions-1.15-gtk3/PKGBUILD
+find ./*/ | grep -v .git | grep -v .SRCINFO | grep -v PKGBUILD | grep -v patch | grep -v aur | grep -v caja-exten > ./list
+b='pkgname="${_pkgbase}-${_ver}-gtk3"' ; for i in `cat ./list` ; do a=$(cat $i/PKGBUILD | grep 'pkgname="${_pkgbase}"') ; if [ ! -z $a ] ; then sed -i -e "s/$a/$b/g" $i/PKGBUILD ; fi ; done
+b='"' ; c='-1.15-gtk3"' ; for i in caja-extensions-common caja-gksu caja-image-converter caja-open-terminal caja-sendto caja-share ; do sed -i -e "s/$i/$b$i$c/g" ./caja-extensions-common-1.15-gtk3/PKGBUILD ; done ; sed -i -e "s/package_\"/package_/g" ./caja-extensions-common-1.15-gtk3/PKGBUILD ; sed -i -e 's/"()/()/g' ./caja-extensions-common-1.15-gtk3/PKGBUILD
 a='>=1.15' ; b='-1.15-gtk3' ; for i in ./*/PKGBUILD ; do sed -i -e "s/$a/$b/g" $i ; done
-#for i in ./*/ ; do echo $i ; cd $i ; makepkg --printsrcinfo > .SRCINFO ; git commit -a -m 'loop' ; git push ; cd .. ; done
+for i in ./*/ ; do cd $i ; makepkg --printsrcinfo > .SRCINFO ;  git commit -av ; git push ; cd .. ; done
