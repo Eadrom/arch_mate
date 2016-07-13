ff424ff5dfdb35007ba6bd976bf411deab85fde2
Νικόλαος Κυριάκος Φυτίλης <n-fit@live.com>
2016-07-13 19:06:05 +0300

    getting closer to be able to upgrade with scripts to the aur

--- aur-env.sh
+++ aur-env.sh
@@ -1,2 +1,4 @@
 mkdir aur ; cd aur
-for i in `find ../*/ | grep -v PKGBUILD | tr -d './'` ; do git clone git+ssh://aur@aur.archlinux.org/$(echo `echo $i | cut -d '/' -f 7`-1.15-gtk3.git) ; done
+for i in `find ../*/ | grep -v .git | grep -v .SRCINFO | grep -v PKGBUILD | grep -v patch | grep -v aur | tr -d './'` ; do git clone git+ssh://aur@aur.archlinux.org/$(echo `echo $i | cut -d '/' -f 7`-1.15-gtk3.git) ; done
+for i in `find ../*/ | grep -v .git | grep -v .SRCINFO | grep -v PKGBUILD | grep -v patch | grep -v aur | tr -d './'` ; do cp -r  ../$i/* ./$(echo $i-1.15-gtk3) ; done
+cd ..
--- fast.sh
+++ fast.sh
@@ -1,5 +1,4 @@
-for i in ../*/ ; do cp -r  $i/* ./$(echo `echo $i | cut -d '/' -f 7`-1.15-gtk3) ; done
-b='pkgname="${_pkgbase}-${_ver}-gtk3"' ; for i in `cat ../list' ; do a=$(cat $i/PKGBUILD | grep 'pkgname="${_pkgbase}"') ; if [ ! -z $a ] ; then sed -i -e "s/$a/$b/g" $i/PKGBUILD ; fi ; done
+b='pkgname="${_pkgbase}-${_ver}-gtk3"' ; for i in `cat ../list` ; do a=$(cat $i/PKGBUILD | grep 'pkgname="${_pkgbase}"') ; if [ ! -z $a ] ; then sed -i -e "s/$a/$b/g" $i/PKGBUILD ; fi ; done
 b='"' ; c='-1.15-gtk3"' ; for i in caja-extensions-common caja-gksu caja-image-converter caja-open-terminal caja-sendto caja-share ; do sed -i -e "s/$i/$b$i$c/g" ./caja-extensions-1.15-gtk3/PKGBUILD ; done ; sed -i -e "s/package_\"/package_/g" ./caja-extensions-1.15-gtk3/PKGBUILD ; sed -i -e 's/"()/()/g' ./caja-extensions-1.15-gtk3/PKGBUILD
 a='>=1.15' ; b='-1.15-gtk3' ; for i in ./*/PKGBUILD ; do sed -i -e "s/$a/$b/g" $i ; done
-for i in ./*/ ; do echo $i ; cd $i ; makepkg --printsrcinfo > .SRCINFO ; git commit -a -m 'loop' ; git push ; cd .. ; done
+#for i in ./*/ ; do echo $i ; cd $i ; makepkg --printsrcinfo > .SRCINFO ; git commit -a -m 'loop' ; git push ; cd .. ; done
