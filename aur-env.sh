aeaf292635bdb31939a57902190c5ac4930a753e
Νικόλαος Κυριάκος Φυτίλης <n-fit@live.com>
2016-07-14 11:19:39 +0300

    this is probably done. further updates with probably be generated from update.sh

--- aur-env.sh
+++ aur-env.sh
@@ -1,5 +1,6 @@
 mkdir aur ; cd aur
 find ../*/ | grep -v mate-themes | grep -v .git | grep -v .SRCINFO | grep -v PKGBUILD | grep -v patch | grep -v aur | tr -d './' > ./list
-for i in `cat list` mate-themes-3.20-gtk3 ; do git clone git+ssh://aur@aur.archlinux.org/$(echo `echo $i | cut -d '/' -f 7`-1.15-gtk3.git) ; done
-for i in `cat list` ; do cp -r  ../$i/* ./$(echo $i-1.15-gtk3) ; done ; cp ../mate-themes/* ./mate-themes-3.20-gtk3
-sh ../fast.sh
+for i in `cat list` ; do git clone git+ssh://aur@aur.archlinux.org/$(echo `echo $i | cut -d '/' -f 7`-1.15-gtk3.git) ; done
+for i in `cat list` ; do cp -r  ../$i/* ./$(echo $i-1.15-gtk3) ; done 
+git clone git+ssh://aur@aur.archlinux.org/mate-themes-3.20-gtk3.git ;  cp ../mate-themes/* ./mate-themes-3.20-gtk3
+#sh ../fast.sh
