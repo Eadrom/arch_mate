df0a5262ef8690d91f95b96ff28261bf38239aea
Νικόλαος Κυριάκος Φυτίλης <n-fit@live.com>
2016-07-28 09:16:15 +0300

    staging for caja-extensions

--- fast.sh
+++ fast.sh
@@ -1,7 +1,7 @@
 find ./*/ | grep -v .git | grep -v .SRCINFO | grep -v PKGBUILD | grep -v patch | grep -v aur | grep -v caja-exten > ./list
-b='pkgname="${_pkgbase}-${_ver}-gtk3"' ; for i in `cat ./list` ; do a=$(cat $i/PKGBUILD | grep 'pkgname="${_pkgbase}"') ; if [ ! -z $a ] ; then sed -i -e "s/$a/$b/g" $i/PKGBUILD ; fi ; done
+c='>=1.15' ; d='-1.15-gtk3' b='pkgname="${_pkgbase}-${_ver}-gtk3"' ; for i in `cat ./list` ; do a=$(cat $i/PKGBUILD | grep 'pkgname="${_pkgbase}"') ; if [ ! -z $a ] ; then sed -i -e "s/$a/$b/g" $i/PKGBUILD ; fi do sed -i -e "s/$c/$d/g" $i ; done
+
 b='"' ; c='-1.15-gtk3"' ; for i in caja-extensions-common caja-gksu caja-image-converter caja-open-terminal caja-sendto caja-share ; do sed -i -e "s/$i/$b$i$c/g" ./caja-extensions-common-1.15-gtk3/PKGBUILD ; done ; sed -i -e "s/package_\"/package_/g" ./caja-extensions-common-1.15-gtk3/PKGBUILD ; sed -i -e 's/"()/()/g' ./caja-extensions-common-1.15-gtk3/PKGBUILD
-a='>=1.15' ; b='-1.15-gtk3' ; for i in ./*/PKGBUILD ; do sed -i -e "s/$a/$b/g" $i ; done
 
 #gen meta package
 a='PKGBUILD' ; for i in ./*/$a ; do echo $i | grep -v meta | sed -e "s/$a//g" | tr -d '/' | sed -e "s/$/'\ /g" | sed -e "s/^./'/g" | tr ' \n' '\\ ' ; done > ./list
