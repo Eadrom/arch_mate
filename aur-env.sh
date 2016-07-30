b676d3c6acc61773f3d2046c18c19c815ee6e150
Νικόλαος Κυριάκος Φυτίλης <n-fit@live.com>
2016-07-30 14:53:46 +0300

    making aur-env faster, updated atril and engrampa

--- aur-env.sh
+++ aur-env.sh
@@ -4,6 +4,7 @@ theme_ver=3.20
 mkdir aur ; cd aur
 find ../*/ | grep -v mate-themes | grep -v .git | grep -v .SRCINFO | grep -v PKGBUILD | grep -v patch | grep -v aur | tr -d './' > ./list
 for i in `cat list` ; do git clone git+ssh://aur@aur.archlinux.org/$(echo `echo $i | cut -d '/' -f 7`-$mate_ver-gtk3.git) ; done
-for i in `cat list` ; do cp -r  ../$i/* ./$(echo $i-$mate_ver-gtk3) ; done 
+for i in `cat list` ; do cp -r  ../$i/* ./$(echo $i-$mate_ver-gtk3) ; done
+#git clone git+ssh://aur@aur.archlinux.org/mate-themes-$theme_ver-gtk3.git ;  cp ../mate-themes/* ./mate-themes-$theme_ver-gtk3
 git clone git+ssh://aur@aur.archlinux.org/mate-themes-$theme_ver-gtk3.git ;  cp ../mate-themes/* ./mate-themes-$theme_ver-gtk3
 sh ../fast.sh
